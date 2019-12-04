6c75d3ba5a2c9993d185683be0c5d05bf7b17bee
RomeroLaura <35375163+RomeroLaura@users.noreply.github.com>
2019-12-04 20:58:34 +0100

    Update KightsC.c

--- KightsC.c
+++ KightsC.c
@@ -1,1 +1,25 @@
 
+#include <stdio.h>
+
+
+struct pair_int_int {
+    int first;
+    int second;
+};
+
+pair_int_int make_pair(conts int & a, const int & b){
+
+  struct pair_int_int apair;
+
+  apair.first = a;
+  apair.second = b;
+
+  return apair;
+
+}
+
+pair_int_int operator+(const pair_int_int & a, const pair_int_int & b){
+
+  return make_pair(a.first + b.first, a.second + b.second);
+
+}
